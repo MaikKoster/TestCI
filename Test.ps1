148cf1ed1531aa9fe4f25c98c9f25fdafe0a8645
Maik Koster <Maik.Koster@gmx.de>
2017-06-01 15:18:51 +0200

    First Pull Request to update Master (#1)
    And here we can merge all the individual commits into a single one for the master branch.

---
+++ Test.ps1
@@ -0,0 +1,10 @@
+Write-Host "Lets apply a couple changes."
+
+Write-Host "And another test."
+
+
+# A comment should be enoug as well.
+
+# and some more changes
+
+# And a final change to create a second pull request.
